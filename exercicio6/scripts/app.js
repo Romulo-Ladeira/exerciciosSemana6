f4922444b47bb5365094d6a66eadb1d295b36299
romuloLadeira <romulo.alesandro@gmail.com>
2021-11-26 19:33:59 -0300

    exercicio-6

---
+++ exercicio6/scripts/app.js
@@ -0,0 +1,9 @@
+let arrayNumeros =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+let arrayQuadrados = []
+
+arrayQuadrados= arrayNumeros.map(elemento=>{
+    return elemento*elemento
+})
+
+console.log(arrayQuadrados)
+
